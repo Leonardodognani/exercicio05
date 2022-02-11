89b11f1d25db5002b445d53ab3a6ee9a4252fbde
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2022-02-11 12:00:32 -0300

    avancing in code

--- Program.cs
+++ Program.cs
@@ -1,11 +1,55 @@
-﻿namespace exercicio05
+﻿/* Desenvolva um algoritmo que solicite a entrada de uma frase, após isto troque todas
+as letras A ou a por &, porém não utilize o método Replace().
+*/
+
+namespace exercicio05
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Program p = new Program();
+
+            string Frase = "O rato roeu a roupa do rei de roma.";
+
+            Console.WriteLine(p.Alterar(Frase));
+            Console.ReadKey();
         }
+
+         string Alterar(string Frase)
+        {
+            if (Frase.Trim().Length > 0)
+            {
+                int Total = Frase.Length;
+                int Count = 0;
+                
+                var StringBuilder = new StringBuilder(Frase);
+
+
+                while (Count < Total)
+                {
+                    if (StringBuilder[Count] == 'a')
+                    {
+                        StringBuilder[Count] = '&';
+                    }
+                    Count++;
+                }
+
+                Frase = StringBuilder.ToString();
+
+                return Frase;
+
+            }
+            else
+            {
+                return null;
+            }
+
+
+        }
+
+
+
     }
     
 }
